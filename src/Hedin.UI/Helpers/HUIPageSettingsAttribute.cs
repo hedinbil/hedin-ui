c2cca0c390b558a1afffc7828395ca197e90b5f7
Martin Almström <martin.almstrom@hedinit.com>
2025-10-08 14:03:21 +0200

    Disabled on HUIPageSettingsAttribute. Clean up as well

--- src/Hedin.UI/Helpers/HUIPageSettingsAttribute.cs
+++ src/Hedin.UI/Helpers/HUIPageSettingsAttribute.cs
@@ -6,11 +6,13 @@ public class HUIPageSettingsAttribute : Attribute
     public string DisplayName { get; }
     public int Order { get; }
     public string? Icon { get; }
+    public bool Disabled { get; }
 
-    public HUIPageSettingsAttribute(string displayName, int order = 99999, string? icon = "")
+    public HUIPageSettingsAttribute(string displayName, int order = 99999, string? icon = "", bool disabled = false)
     {
         DisplayName = displayName;
         Order = order;
         Icon = icon;
+        Disabled = disabled;
     }
 }
